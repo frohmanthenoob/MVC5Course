e346bc65080eb2f3fd21ee4885cac8d61d57320c
frohmanthenoob <cabal_357@hotmail.com>
2017-03-26 17:20:30 +0800

    end

--- MVC5Course/App_Start/WebApiConfig.cs
+++ MVC5Course/App_Start/WebApiConfig.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.OData.Builder;
+using System.Web.Http.OData.Extensions;
+using MVC5Course.Models;
 
 namespace MVC5Course
 {
@@ -10,7 +13,10 @@ namespace MVC5Course
         public static void Register(HttpConfiguration config)
         {
             // Web API 設定和服務
-
+            //ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            //builder.EntitySet<Product>("ProductsAPI");
+            //builder.EntitySet<OrderLine>("OrderLine");
+            //config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
             // Web API 路由
             config.MapHttpAttributeRoutes();
 
